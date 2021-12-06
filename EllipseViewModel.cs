30395f1de4f7adb95350f0c4d0c4dfe4cc36c4f0
Jason Goemaat <jason.goemaat@gmail.com>
2021-12-06 00:41:11 -0600

    Wokinf with my ellipses

---
+++ EllipseViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace GameTestApp
+{
+    // https://social.msdn.microsoft.com/Forums/sqlserver/en-US/83376231-1d93-47b5-acc3-2fa51f151c1f/how-to-add-children-to-a-canvas-dynamically-in-mvvm?forum=wpf
+
+    public class EllipseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private double x;
+
+        public double X
+        {
+            get => x;
+            set
+            {
+                this.x = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("X"));
+            }
+        }
+
+        private double y;
+
+        public double Y
+        {
+            get => y;
+            set
+            {
+                this.y = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Y"));
+            }
+        }
+
+        private double radius;
+
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                this.radius = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Radius"));
+            }
+        }
+
+        private Color color;
+
+        public Color Color
+        {
+            get => color;
+            set
+            {
+                this.color = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Color"));
+            }
+        }
+    }
+}
---
+++ MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace wpf_Generic_ListBox_Canvas
+{
+    public class MainWindowViewModel
+    {
+        public ObservableCollection<BaseThing> Items { get; set; }
+        public MainWindowViewModel()
+        {
+            Items = new ObservableCollection<BaseThing>
+            {
+                new CircleVM {Top=000.0, Left=000.0, EllipseHeight=400, EllipseWidth=400, Thickness = 40, Color = Color.FromArgb(255, 255, 0, 0)},
+                new CircleVM {Top=040.0, Left=040.0, EllipseHeight=320, EllipseWidth=320, Thickness = 40, Color = Color.FromArgb(255, 0, 0, 255)},
+                new CircleVM {Top=080.0, Left=080.0, EllipseHeight=240, EllipseWidth=240, Thickness = 40, Color = Color.FromArgb(255, 0, 255, 0)}
+            };
+        }
+    }
+
+    public class BaseThing
+    {
+        public Double Left { get; set; }
+        public Double Top { get; set; }
+        public Double Height { get; set; }
+        public Double Width { get; set; }
+    }
+
+    public class CircleVM : BaseThing
+    {
+        public Double EllipseHeight { get; set; }
+        public Double EllipseWidth { get; set; }
+        public Color Color { get; set; }
+        public Double Thickness { get; set; }
+    }
+
+    class TextBoxVM : BaseThing
+    {
+        public string TbText { get; set; }
+    }
+}
